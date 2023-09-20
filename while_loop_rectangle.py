b0435fc53ec2f3f87690266c5c2b9f1cc88f8bac
Shyamsunder Merugu <shyamsundermerugu@gmail.com>
2023-09-20 08:44:00 +0530

    first commit

---
+++ while_loop_rectangle.py
@@ -0,0 +1,6 @@
+rows = int(input('Enter number of rows: '))
+cols = int(input('Enter number of columns: '))
+counter = 1 
+while counter <= 3:
+    print((str(counter)+' ')*cols)
+    counter = counter + 1 
